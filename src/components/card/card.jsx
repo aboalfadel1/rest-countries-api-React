272862cbde8f6d6d33b404e2e6f2368ba03366c3
Mohammad Zarzour <medosaad18@gmail.com>
2025-05-17 23:39:04 +0400

    syria free

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styles from './card.module.css'
 import { Link } from 'react-router-dom';
 import { useDarkMode } from '../../store/darkMode';
+import newSyria from "../../imgs/syria.svg.png"
 
 export default function Card({name,population,region,capital,flag}) {
   const {isDarkMode} = useDarkMode()
   return (
     <Link to={name} className= {`${styles.card} ${isDarkMode ? "dark-mode":""}`}>
-       <div className={styles.image}> <img src={flag} alt="flag" /></div>
+       <div className={styles.image}> <img src={name==="Syria" ?newSyria:flag} alt="flag" /></div>
         <div className={styles.text}>
             <h3>{name}</h3>
             <p>Population: {population}</p>
