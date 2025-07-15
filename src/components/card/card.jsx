0a16ef449e5c45026f14ce0e0db9e68ce4167b46
Mouayad Saad <medosaad18@gmail.com>
2025-07-15 11:49:02 +0200

    updated darkmode in card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import styles from './card.module.css'
-import { Link } from 'react-router-dom';
-import { useDarkMode } from '../../store/darkMode';
-import newSyria from "../../imgs/syria.svg.png"
+import React from "react";
+import styles from "./card.module.css";
+import { Link } from "react-router-dom";
+import { useDarkMode } from "../../store/darkMode";
+import newSyria from "../../imgs/syria.svg.png";
 
-export default function Card({name,population,region,capital,flag}) {
-  const {isDarkMode} = useDarkMode()
+export default function Card({ name, population, region, capital, flag }) {
+  const { isDarkMode } = useDarkMode();
   return (
-    <Link to={name} className= {`${styles.card} ${isDarkMode ? "dark-mode":""}`}>
-       <div className={styles.image}> <img src={name==="Syria" ?newSyria:flag} alt="flag" /></div>
-        <div className={styles.text}>
-            <h3>{name}</h3>
-            <p>Population: {population}</p>
-            <p>Region: {region}</p>
-            <p>Capital: {capital}</p>
-        </div>
+    <Link
+      to={name}
+      className={`${styles.card} ${isDarkMode ? "dark-mode" : ""}`}
+    >
+      <div className={styles.image}>
+        {" "}
+        <img src={name === "Syria" ? newSyria : flag} alt="flag" />
+      </div>
+      <div className={isDarkMode ? styles.textDark:styles.text}>
+        <h3>{name}</h3>
+        <p>Population: {population}</p>
+        <p>Region: {region}</p>
+        <p>Capital: {capital}</p>
+      </div>
     </Link>
-  )
+  );
 }
